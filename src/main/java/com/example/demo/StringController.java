3611fe869d94f761850b4ddd10925f6d4e5a4a6e
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-10-27 15:12:12 +0100

    Implemented method to collect data about strings.

--- src/main/java/com/example/demo/StringController.java
+++ src/main/java/com/example/demo/StringController.java
@@ -1,4 +1,32 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StringController {
+
+    @GetMapping("/{sequence}")
+    public StringData stringInfo(@PathVariable("sequence") String sequence) {
+
+        int numbers = 0;
+        int upperCase = 0;
+        int lowerCase = 0;
+        int specialCharacters = 0;
+
+        for (int i = 0; i < sequence.length(); i++) {
+
+            if (Character.isDigit(sequence.charAt(i))){
+                numbers++;
+            }else if (Character.isLowerCase(sequence.charAt(i))){
+                lowerCase++;
+            }else if (Character.isUpperCase(sequence.charAt(i))){
+                upperCase++;
+            }else {
+                specialCharacters++;
+            }
+        }
+        return new StringData(lowerCase, upperCase, numbers, specialCharacters);
+    }
 }
